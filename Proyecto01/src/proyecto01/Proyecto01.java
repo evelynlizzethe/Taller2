fd661ff7e35657dc76b4bf00e1f8e07c9bf14348
evelynlizzethe <elriofrio1@utpl.edu.ec>
2024-10-17 20:17:13 -0500

    segundo commit

--- Proyecto01/src/proyecto01/Proyecto01.java
+++ Proyecto01/src/proyecto01/Proyecto01.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package proyecto01;
-
-/**
- *
- * @author UTPL
- */
 public class Proyecto01 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Ejemplo de Java "
+                + "manejado desde GitHub "
+                + "versionado a través de Git "
+                + "creado por evelynlizzethe");
     }
     
 }
